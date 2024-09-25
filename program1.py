4fd7632ca35d728697293683e6f6cbe0739c69b4
gouravghoshi55 <149483422+gouravghoshi55@users.noreply.github.com>
2024-09-25 14:14:25 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,18 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
 
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map.keys():
+                if stack == [] or bracket_map[char] != stack.pop():
+                    return False
 
+        return stack == []
 
-
-
-
-
-    
-
-
-
-  
-
+# Example usage
+solution = Solution()
+user_input = "" 
+print(solution.isValid(user_input))
