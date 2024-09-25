b472a904cc7ec08fb50b7cc991ff580bb9468d39
gouravghoshi55 <149483422+gouravghoshi55@users.noreply.github.com>
2024-09-25 14:09:37 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,10 +1,27 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        roman = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            if i < n - 1 and roman[s[i]] < roman[s[i + 1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+        
+        return total
+
+# Example usage:
+solution = Solution()
+print(solution.romanToInt("III"))      # Output: 3
+print(solution.romanToInt("LVIII"))    # Output: 58
+print(solution.romanToInt("MCMXCIV"))  # Output: 1994
+
 
 
 
